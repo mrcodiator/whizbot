7776be28cb1f556da61fe220ebdabe00edae092d
mrcodiator <mr.codiator@gmail.com>
2024-10-29 08:33:38 +0530

    bug fix #2

--- src/helper/response.ts
+++ src/helper/response.ts
@@ -1,4 +1,11 @@
-import { IResponse } from "@/types/api.types";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export interface IResponse<T = any> {
+    status: number;
+    message: string;
+    success: boolean;
+    data?: T;
+}
 
 export async function sendResponse<T>(res: IResponse, data?: T): Promise<IResponse<T>> {
     return { status: res.status, message: res.message, success: res.success, data };
