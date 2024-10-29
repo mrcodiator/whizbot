fa21d65a4f378d4a10b655613112fc3035b23e37
mrcodiator <mr.codiator@gmail.com>
2024-10-29 08:48:50 +0530

    bug fix #5

--- src/hooks/use-create-customer.ts
+++ src/hooks/use-create-customer.ts
@@ -1,49 +1,49 @@
-import { createCustomer } from "@/actions/customer.actions"
-import { createCustomerSchema } from "@/schemas/customer.schema"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useState } from "react"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { toast } from "./use-toast"
+// import { createCustomer } from "@/actions/customer.actions"
+// import { createCustomerSchema } from "@/schemas/customer.schema"
+// import { zodResolver } from "@hookform/resolvers/zod"
+// import { useState } from "react"
+// import { useForm } from "react-hook-form"
+// import { z } from "zod"
+// import { toast } from "./use-toast"
 
-export const useCreateCustomer = () => {
-    const [open, setOpen] = useState(false)
-    const [loading, setLoading] = useState(false)
+// export const useCreateCustomer = () => {
+//     const [open, setOpen] = useState(false)
+//     const [loading, setLoading] = useState(false)
 
-    const form = useForm({
-        resolver: zodResolver(createCustomerSchema),
-        defaultValues: {
-            name: "",
-            email: "",
-            domainId: "",
-        }
-    })
+//     const form = useForm({
+//         resolver: zodResolver(createCustomerSchema),
+//         defaultValues: {
+//             name: "",
+//             email: "",
+//             domainId: "",
+//         }
+//     })
 
-    const onSubmit = async (values: z.infer<typeof createCustomerSchema>) => {
-        console.log(values);
+//     const onSubmit = async (values: z.infer<typeof createCustomerSchema>) => {
+//         console.log(values);
 
-        setLoading(true)
-        try {
-            const res = await createCustomer(values);
+//         setLoading(true)
+//         try {
+//             const res = await createCustomer(values);
 
-            if (res.success) {
-                setOpen(false);
-                form.reset();
-                toast({ title: res.message })
-            } else {
-                toast({ title: res.message, variant: "destructive" })
-            }
-        } catch (error) {
-            console.log(error);
-            toast({
-                description: "Something went wrong",
-                title: "Error",
-                variant: "destructive"
-            })
-        } finally {
-            setLoading(false)
-        }
-    }
+//             if (res.success) {
+//                 setOpen(false);
+//                 form.reset();
+//                 toast({ title: res.message })
+//             } else {
+//                 toast({ title: res.message, variant: "destructive" })
+//             }
+//         } catch (error) {
+//             console.log(error);
+//             toast({
+//                 description: "Something went wrong",
+//                 title: "Error",
+//                 variant: "destructive"
+//             })
+//         } finally {
+//             setLoading(false)
+//         }
+//     }
 
-    return { form, onSubmit, loading, open, setOpen }
-}
+//     return { form, onSubmit, loading, open, setOpen }
+// }
